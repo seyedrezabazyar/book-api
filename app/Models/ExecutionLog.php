8abda526100234e73253ca3e739088411c8e78c2
seyedrezabazyar <seyedrezabazyar@gmail.com>
2025-06-08 17:10:51 +0330

    change api scraper 3

--- app/Models/ExecutionLog.php
+++ app/Models/ExecutionLog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
 
 class ExecutionLog extends Model
 {
